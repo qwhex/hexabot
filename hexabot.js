7224d37b1994dcd210617298b3c0b7516286e330
Mice Papai <16381622+qwhex@users.noreply.github.com>
2018-04-30 16:07:25 +0200

    Generate color images

--- hexabot.js
+++ hexabot.js
@@ -2,6 +2,12 @@ const Telegraf = require('telegraf')
 const Color = require('color')
 const namedColors = require('color-name-list')
 const fuzzy = require('fuzzy')
+const window = require('svgdom')
+const SVG = require('svg.js')(window)
+const document = window.document
+const pn = require('pn/fs')
+const fs = require('fs')
+const svg2png = require('svg2png')
 
 const nearestColorName = (() => {
   let colors = {}
@@ -20,21 +26,6 @@ const INVALID = 2
 function makeBot () {
   const bot = new Telegraf(process.env.HEXABOT_KEY)
 
-  bot.on('text', (ctx) => {
-    const query = ctx.message.text
-
-    const colorHex = validateColor(query)
-
-    let result
-    if (colorHex === null) {
-      result = searchByName(query)
-    } else {
-      result = searchByHex(query)
-    }
-
-    makeResponse(ctx, result)
-  })
-
   bot.use(async (ctx, next) => {
     const start = new Date()
     await next()
@@ -46,8 +37,21 @@ function makeBot () {
     console.log('Ooops', err)
   })
 
-  bot.startPolling()
+  bot.on('text', (ctx) => {
+    const query = ctx.message.text
+    const colorHex = validateColor(query)
 
+    let result
+    if (colorHex === null) {
+      result = searchByName(query)
+    } else {
+      result = searchByHex(colorHex)
+    }
+
+    makeResponse(ctx, result)
+  })
+
+  bot.startPolling()
   return bot
 }
 
@@ -56,14 +60,8 @@ function validateColor (color) {
   return matches !== null ? matches[1] : null
 }
 
-function searchByHex (rawColor) {
-  let rawColorHex = validateColor(rawColor)
-  if (rawColorHex === null) {
-    return searchByName(rawColor)
-  }
-
+function searchByHex (rawColorHex) {
   const color = Color('#' + rawColorHex)
-
   const [r, g, b] = color.rgb().array()
   const colorHex = rgbToHex(r, g, b)
 
@@ -81,36 +79,57 @@ function rgbToHex (r, g, b) {
 }
 
 function searchByName (colorName) {
-  const results = fuzzy.filter(colorName, namedColors, {
-    extract: function (el) { return el.name }
-  })
+  const results = fuzzy.filter(
+    colorName,
+    namedColors,
+    { extract: (el) => el.name }
+  )
 
   if (results[0] === undefined) {
     return [colorName, 'Invalid color', INVALID]
   }
 
   const match = namedColors[results[0].index]
-  return [
-    match.hex,
-    match.name,
-    match.name.toLowerCase() === colorName.toLowerCase() ? EXACT : APPROX
-  ]
+  const matchType = match.name.toLowerCase() === colorName.toLowerCase() ? EXACT : APPROX
+  return [match.hex, match.name, matchType]
 }
 
 function makeResponse (ctx, result) {
+  // const senderId = ctx.message.from.id
+
   switch (result[2]) {
     case EXACT:
+      makeImageResponse(ctx, result[0])
       return ctx.replyWithHTML(`Exact match: <b>${result[1]}</b> (${result[0]})`)
     case APPROX:
+      makeImageResponse(ctx, result[0])
       return ctx.reply(`Closest match: ${result[1]} (${result[0]})`)
     case INVALID:
       return ctx.reply(`Not found: ${result[1]} (${result[0]})`)
     default:
-      return ctx.replyWithPhoto({
-        url: 'https://picsum.photos/200/300/?random',
-        filename: 'kitten.jpg'
-      })
+      return ctx.reply(`🤷`)
   }
+}
+
+function makeImageResponse (ctx, colorHex) {
+  const imagePath = `cache/${colorHex}.png`
+
+  if (fs.existsSync(imagePath)) {
+    return ctx.replyWithPhoto({ source: imagePath })
+  }
+
+  svg2png(generateSvg(colorHex), { width: 500, height: 500 })
+    .then(buffer => {
+      pn.writeFile(imagePath, buffer)
+      ctx.replyWithPhoto({ source: imagePath })
+    })
+    .catch(e => console.error(e))
+}
+
+function generateSvg (colorHex) {
+  let draw = SVG(document.documentElement).size(500, 500)
+  draw.circle(400).fill(colorHex).move(50, 50)
+  return draw.svg()
 }
 
 makeBot()
