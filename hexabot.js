d38c284c5ffb242194dd18acd52d82868950563f
Mice Papai <16381622+qwhex@users.noreply.github.com>
2018-05-05 16:31:59 +0200

    Hsl bars

--- hexabot.js
+++ hexabot.js
@@ -71,7 +71,7 @@ function extractColors (ctx, imgPath) {
 
   const extractedColors = Colibri.extractImageColors(img, 'css')
   for (let contentColor of extractedColors.content) {
-    makeResponse(ctx, searchByHex(rgbToHex(Color(contentColor)).slice(1)))
+    makeResponse(ctx, searchByHex(colorToHex(Color(contentColor)).slice(1)))
   }
 }
 
@@ -82,7 +82,7 @@ function validateColor (color) {
 
 function searchByHex (rawColorHex) {
   const color = Color('#' + rawColorHex)
-  const colorHex = rgbToHex(color)
+  const colorHex = colorToHex(color)
 
   const exactMatch = namedColors.find(color => color.hex === colorHex)
   if (exactMatch !== undefined) {
@@ -113,12 +113,12 @@ function makeResponse (ctx, result) {
   switch (result[2]) {
   case EXACT:
     makeImageResponse(ctx, result)
-    return ctx.reply(`Exact match: ${result[1]} (${result[0]})`)
+    return ctx.reply(`Exact match: ${result[1]} ${result[0]}`)
   case APPROX:
     makeImageResponse(ctx, result)
-    return ctx.reply(`Closest match: ${result[1]} (${result[0]})`)
+    return ctx.reply(`Closest match: ${result[1]} ${result[0]}`)
   case INVALID:
-    return ctx.reply(`Not found: ${result[1]} (${result[0]})`)
+    return ctx.reply(`🤷 ${result[1]} ${result[0]}`)
   default:
     return ctx.reply(`🤷`)
   }
@@ -126,16 +126,27 @@ function makeResponse (ctx, result) {
 
 function makeImageResponse (ctx, result) {
   const [colorHex, colorName] = result.slice(0, 2)
-  const imagePath = path.resolve(__dirname, `./cache/${colorHex}.png`)
+  const filename = `${colorHex}.png`
+  const imagePath = path.resolve(__dirname, `./cache/${filename}`)
 
   if (fs.existsSync(imagePath)) {
-    return ctx.replyWithPhoto({ source: imagePath })
+    return ctx.replyWithPhoto({
+      source: imagePath,
+      filename: filename,
+      caption: colorName,
+      reply_to_message_id: ctx.message.message_id
+    })
   }
 
   svg2png(generateSvg(colorHex, colorName))
     .then(buffer => {
       pn.writeFile(imagePath, buffer)
-        .then(() => ctx.replyWithPhoto({ source: imagePath }))
+        .then(() => ctx.replyWithPhoto({
+          source: imagePath,
+          filename: filename,
+          caption: colorName,
+          reply_to_message_id: ctx.message.message_id
+        }))
         .catch(err => console.error(err))
     })
     .catch(err => console.error(err))
@@ -143,12 +154,13 @@ function makeImageResponse (ctx, result) {
 
 function generateSvg (colorHex, colorName) {
   const color = Color(colorHex)
-  const bgColor = rgbToHex(getBgColor(color))
+  const bgColor = colorToHex(getBgColor(color))
 
   let draw = SVG(document.documentElement).size(850, 700)
   drawColorCircle(draw, colorHex, bgColor)
   drawTitle(draw, colorHex, colorName)
   drawRgbBars(draw, color)
+  drawHslBars(draw, color)
   return draw.svg()
 }
 
@@ -164,19 +176,19 @@ function getBgColor (color) {
 
 function drawColorCircle (draw, fgColorHex, bgColorHex) {
   draw.rect(950, 700).fill(bgColorHex)
-  draw.circle(400).fill(fgColorHex).move(50, 50)
+  draw.circle(400).fill(fgColorHex).move(50, 150)
   return draw
 }
 
 function drawTitle (draw, fgColorHex, colorName) {
   const fontConfig = {
     fill: fgColorHex,
-    family: 'Quicksand',
+    family: 'Andale Mono',
     size: '40px'
   }
 
   let colorNameText = draw.text(colorName)
-  colorNameText.move(50, 500).font(fontConfig)
+  colorNameText.move(50, 50).font(fontConfig)
 
   let colorHexText = draw.text(fgColorHex)
   colorHexText.move(50, 575).font(fontConfig)
@@ -185,26 +197,44 @@ function drawTitle (draw, fgColorHex, colorName) {
 }
 
 function drawRgbBars (draw, color) {
-  const height = 250
+  const height = 275
   const rgb = color.rgb().array()
   const heights = rgb.map(attribute => {
     return Math.round((attribute / 255) * height)
   })
 
   draw.rect(50, heights[0])
-    .fill(rgbToHex(Color.rgb(rgb[0], 0, 0)))
+    .fill(colorToHex(Color.rgb(rgb[0], 0, 0)))
     .move(550, 50)
   draw.rect(50, heights[1])
-    .fill(rgbToHex(Color.rgb(0, rgb[1], 0)))
+    .fill(colorToHex(Color.rgb(0, rgb[1], 0)))
     .move(650, 50)
   draw.rect(50, heights[2])
-    .fill(rgbToHex(Color.rgb(0, 0, rgb[2])))
+    .fill(colorToHex(Color.rgb(0, 0, rgb[2])))
     .move(750, 50)
 
   return draw
 }
 
-function rgbToHex (color) {
+function drawHslBars (draw, color) {
+  const height = 275
+  const hsl = color.hsl().array()
+  const colorHex = colorToHex(color)
+
+  draw.rect(50, Math.round((hsl[0] / 360) * height))
+    .fill(colorHex)
+    .move(550, 375)
+  draw.rect(50, Math.round((hsl[1] / 100) * height))
+    .fill(colorHex)
+    .move(650, 375)
+  draw.rect(50, Math.round((hsl[2] / 100) * height))
+    .fill(colorHex)
+    .move(750, 375)
+
+  return draw
+}
+
+function colorToHex (color) {
   const [r, g, b] = color.rgb().array()
   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
 }
