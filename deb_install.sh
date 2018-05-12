4bb5f2b81dbd9b9c7b30b44bb1734121548085af
Mice Pápai <gothart.hu@gmail.com>
2018-05-12 14:33:17 +0200

    Add shebang

--- deb_install.sh
+++ deb_install.sh
@@ -1,1 +1,7 @@
-apt-get install libcairo2-dev libjpeg-dev libgif-dev
+#!/bin/sh
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt-get install -y nodejs
+
+apt-get update
+apt-get install -y libcairo2-dev libjpeg-dev libgif-devS
