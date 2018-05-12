7886bda62f3db568caa3a078979a1a44e72a191e
qwhex <16381622+qwhex@users.noreply.github.com>
2018-05-12 21:18:27 +0200

    Update deb_install.sh

--- deb_install.sh
+++ deb_install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-apt-get install -y nodejs
-
-apt-get update
-apt-get install -y libcairo2-dev libjpeg-dev libgif-devS
+apt-get -qq update
+apt-get install -y libcairo2-dev libjpeg-dev libgif-dev
